08f2c32efa19f60bf45436d0143ca12e7596b7b8
X5tar <coldstar.hx@gmail.com>
2020-02-21 19:57:51 +0800

    Update level0/bubbleSort

--- level0/bubbleSort/bubbleSort.h
+++
@@ -1,6 +0,0 @@
-#ifndef BUBBLESORT_BUBBLESORT_H
-#define BUBBLESORT_BUBBLESORT_H
-
-void bubbleSort(int *array, int length);
-
-#endif //BUBBLESORT_BUBBLESORT_H
--- level0/bubbleSort/main.c
+++ level0/bubbleSort/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "bubbleSort.h"
 #define LENGTH 10
+void bubbleSort(int *array, int length);
 
 int main(void) {
     int array[LENGTH] = {3, 5, 1, 12, 0, -3, 25, 1, 2, -13};
@@ -18,4 +18,16 @@ int main(void) {
     printf("\n");
 
     return 0;
+}
+
+void bubbleSort(int *array, int length) {
+    for (int i = 0; i < length; i++) {
+        for (int j = i + 1; j < length; j++) {
+            if (array[i] > array[j]) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
 }
