ecc8db3c22ea3189dd064b6fb4771ec59f92ff64
sorarain <57026826+sorarain@users.noreply.github.com>
2020-02-25 16:45:28 +0800

    Update linkedList.cpp

--- level1/p11_linkedList/linkedList.cpp
+++ level1/p11_linkedList/linkedList.cpp
@@ -31,6 +31,9 @@ void build()
 	FILE *fin;
 	
 	fin = fopen("data.txt","r");
+	if(fin == NULL) printf("Î´ÕÒµ½data.txtÎÄ¼þÇëÊÖ¹¤ÊäÈë\n"),fin = stdin;
+	if(fin == NULL) printf("ÇëÊäÈë²åÈëµÄ½Úµã×ÜÊý\n");
+	if(fin == NULL) printf("ÇëÊäÈë²åÈëµÄ½Úµã±àºÅºÍÊýÄ¿\n");
 	fscanf(fin,"%d", &n);
 	for(i = 1;i <= n;i++)
 	{
@@ -38,15 +41,17 @@ void build()
 		fscanf(fin,"%d%d", &num, &val);
 		insert(val, num);
 	}
+	if(fin != NULL) printf("successful\n");
+	system("cls");
 }
 
 void pf()
 {
-	printf("\n");
+	printf("Á´±í¸÷½ÚµãµÄÐÅÏ¢ÈçÏÂ\n");
 	node* p = rt;
 	while(p!=NULL)
 	{
-		printf("val:%d num:%d\n", p->val, p->num);
+		printf("val:%d id:%d\n", p->val, p->num);
 		p = p->nxt;
 	}
 }
@@ -76,18 +81,21 @@ void find()
 		p = p->nxt;
 	}
 	if(!flag)
-	printf("-1");
+	printf("Î´ÕÒµ½ÖµÎª5µÄÔªËØ");
 }
 int main(void)
 {
 	rt = back = NULL;
+	printf("´Ódata.txtÖÐµ¼ÈëÁ´±í\n");
 	build();
 	while(1)
 	{
+		printf("1Ê¹Á´±í·´Ðò²¢Êä³ö\n2Êä³öÁ´±íËùÓÐÔªËØ\n3²éÕÒÖµÎª5µÄÔªËØ\n4ÍË³ö³ÌÐò\n");
 		char c = getch();
 		if(c == '1')
 		{
 			rev(rt, NULL);
+			printf("·´Ðòºó");
 			pf();
 		}
 		else if(c == '2')
@@ -98,5 +106,9 @@ int main(void)
 		{
 			find();
 		}
+		else if(c == '4') exit(0);
+		printf("°´ÈÎÒâ¼ü¼ÌÐø\n");
+		char ch = getch();
+		system("cls");
 	}
 }
