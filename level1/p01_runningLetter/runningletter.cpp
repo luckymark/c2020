1fe7ddfe3db26378a8c96eb9d5074d4ed79df64e
abigarrtist <2069460766@qq.com>
2020-02-25 22:43:26 +0800

    text 1

---
+++ level1/p01_runningLetter/runningletter.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+const int wide = 10;
+int main() {
+	int i = 0;
+	while (1) {
+		for (i; i <= wide; i++) {
+			for (int j = 0; j <= i; j++) printf(" ");
+			printf("Hello, World!");
+			system("cls");
+			//Sleep(2000);
+		}
+		i -= 1;
+		for (i; i >= 0; i--) {
+			for (int j = 0; j <= i; j++) printf(" ");
+			printf("Hello, World!");
+			system("cls");
+			//Sleep(2000);
+		}
+	}
+	return 0;
+}
