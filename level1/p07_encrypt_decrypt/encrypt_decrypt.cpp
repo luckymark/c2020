5483f54c7e967c7baf13b6933995f4e2e12340ce
sorarain <57026826+sorarain@users.noreply.github.com>
2020-02-26 18:49:03 +0800

    Update encrypt_decrypt.cpp

--- level1/p07_encrypt_decrypt/encrypt_decrypt.cpp
+++ level1/p07_encrypt_decrypt/encrypt_decrypt.cpp
@@ -31,3 +31,28 @@ void decrypt(char* s)
 	for(i = 0;i < len;i++)
 	printf("%c", *(s + i) - 1 - i / 2);
 }
+int main(void)
+{
+	while(1)
+	{
+		system("cls");
+		printf("ÇëÑ¡ÔñÒªÊµÏÖµÄ¹¦ÄÜ\n");
+		printf("1¼ÓÃÜ×Ö·û´®\n");
+		printf("2½âÃÜ×Ö·û´®\n");
+		char c;
+		c = getch();
+		if(c != '1' && c!= '2') continue;
+		if(c == '1')
+		printf("ÇëÊäÈëÒª¼ÓÃÜµÄ×Ö·û´®\n");
+		else if(c == '2')
+		printf("ÇëÊäÈëÒª½âÃÜµÄ×Ö·û´®\n");
+		char s[200];
+		scanf("%s",s);
+		if(c == '1')
+		encrypt(s);
+		else decrypt(s);
+		printf("\nÇë°´ÈÎÒâ¼ü¼ÌÐø");
+		char ch = getch();
+	}
+	return 0;
+}
