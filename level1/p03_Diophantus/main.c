e325b9dc5bdb022ca774163738b3c99e1fb57e22
ουονοωο <ouovowo@outlook.com>
2020-02-26 21:23:12 +0800

    Update main.c

--- level1/p03_Diophantus/main.c
+++ level1/p03_Diophantus/main.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+	system("title Diophantus");
+	const float percent = .5 - 1. / 6 - 1. / 12 - 1. / 7;
+	printf("%d\n", (int)((5. + 4.) / percent - 4. + .5));
+	system("pause");
+	return 0;
+}
