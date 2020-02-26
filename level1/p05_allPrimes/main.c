46d0da600650b48b8f1b9a4c0fdf4c8e3a33bc0b
ουονοωο <ouovowo@outlook.com>
2020-02-26 22:09:09 +0800

    Update main.c

--- level1/p05_allPrimes/main.c
+++ level1/p05_allPrimes/main.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+#define MAXN 1000
+int main() {
+    int book[MAXN + 1] = {0};
+    for (int i = 2; i <= MAXN; i++) {
+        if (!book[i]) printf("%d\n", i);
+        for (int j = i; j <= MAXN; j += i) book[j] = 1;
+    }
+    return 0;
+}
