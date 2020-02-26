493cb0a22483bd3436083223951f0650376ffa03
sorarain <57026826+sorarain@users.noreply.github.com>
2020-02-26 17:54:18 +0800

    Update narcissus.cpp

--- level1/p04_ narcissus/narcissus.cpp
+++ level1/p04_ narcissus/narcissus.cpp
@@ -1,11 +1,25 @@
 #include<stdio.h>
+const int minn = 100;
+const int maxn = 999;
+const int a = 3;
+int pw(int a, int b)
+{
+	int ans = 1, t = a;
+	while(b)
+	{
+		if(b & 1) ans *= t;
+		t = t * t;
+		b >>= 1;
+	}
+	return ans;
+}
 bool judge(int n)
 {
 	int x = 0, y = n;
 	while(n)
 	{
 		int z = n % 10;
-		x += z * z * z;
+		x += pw(z, a);
 		n /=10;
 	}
 	return y==x;
@@ -14,7 +28,7 @@ bool judge(int n)
 int main(void)
 {
 	int i;
-	for(i = 100;i <= 999;i++)
+	for(i = minn;i <= maxn;i++)
 	if(judge(i))
 	printf("%d ", i);
 	return 0;
