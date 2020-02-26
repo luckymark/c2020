c914c796c635e8fbe97d565e45baad8ed1f93c4d
ουονοωο <ouovowo@outlook.com>
2020-02-26 21:38:04 +0800

    Update main.c

--- level1/p04_ narcissus/main.c
+++ level1/p04_ narcissus/main.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main() {
+	for (int i = 1; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			for (int k = 0; k < 10; k++)
+				if (i*i*i+j*j*j+k*k*k == i*100+j*10+k)
+					printf("%d\n", i * 100 + j * 10 + k);
+	return 0;
+}
