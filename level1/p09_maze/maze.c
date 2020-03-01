76bca1f713ebfbd39d014f83a6159b53176a6cb4
itungsten <tungstenig@gmail.com>
2020-03-01 11:32:46 +0800

    在代大佬的教导下，maze改用prime算法生成迷宫 为了打印方框字符，使用了ascii码3，可能具体实现与机器有关

--- level1/p09_maze/maze.c
+++ level1/p09_maze/maze.c
@@ -1,95 +1,68 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<conio.h>
-#define MAXN 15
-bool maze[MAXN][MAXN];
+#include"maze.h"
 int i;
-int main(){ 
-    for(i=0;i<MAXN;++i){ 
-        maze[0][i]=1;
-    }
-    for(i=0;i<MAXN;++i){ 
-        maze[i][0]=1;
-    }
-    for(i=0;i<MAXN;++i){ 
-        maze[i][14]=1;
-    }
-    for(i=0;i<MAXN;++i){ 
-        maze[14][i]=1;
-    }
-    for(i=0;i<10;++i)maze[3][i]=1;
-    for(i=3;i<6;++i)maze[i][9]=1;
-    maze[5][10]=maze[5][11]=1;
-    for(i=0;i<7;++i)maze[7][i]=maze[7][8+i]=1;
-    for(i=8;i<11;++i)maze[i][6]=1;
-    for(i=7;i<12;++i)maze[10][i]=1; 
-    for(i=10;i>8;--i)maze[i][11]=1;
-    maze[14][7]=0;
-    maze[0][7]=0;
-    //make baffle
-
-    int x=7,y=14;
-    //Your Position
-
+int main(){
     int temp,j;
-    while(1){ 
+    createMaze();
+    while(1){
         system("cls");
-    	//flush frame
-        for(i=0;i<MAXN;++i){ 
+        //flush frame
+        for(i=0;i<MAXN;++i){
             for(j=0;j<MAXN;++j){
-                if(maze[i][j]){ 
-                    printf("*");
+                if(maze[i][j]){
+                    printf("%c ",3);
                     //baffle
-                } 
-                else if(i==y&&j==x)printf("A");
-                //You are Here
-                else if(i==0&&j==7)printf("B");
-                //Destination
-                else printf(" ");
+                }
+                else if(i==aY&&j==aX)printf("A ");
+                    //aYou are Here
+                else if(i==2&&j==1)printf("B ");
+                    //Destination
+                else printf("  ");
                 //road
             }
             printf("\n");
-        } 
+        }
         puts("\n!!Using WSAD to get B!!\n");
         //instruction
-        if(x==7&&y==0){
-        	goto end;
-        	//win
-		}
+        if(aX==1&&aY==2){
+            goto end;
+            //win
+        }
         temp=getch();
-        
-        switch(temp){ 
 
-         case 'w':
-             if((y>0)&&(maze[y-1][x]!=1)){ 
-                y-=1;
-             }
-             break;
+        switch(temp){
 
-         case 's':
-             if(((y<14)&&(maze[y+1][x]!=1)))
-             { 
-                y+=1;
-             }
-             break;
+            case 'w':
+                if((aY>1)&&(maze[aY-1][aX]!=1)){
+                    aY-=1;
+                }
+                break;
 
-         case 'a':
-             if((x>0)&&maze[y][x-1]!=1){ 
-                x-=1;
-             }
-             break;
+            case 's':
+                if(((aY<MAXN-2)&&(maze[aY+1][aX]!=1)))
+                {
+                    aY+=1;
+                }
+                break;
 
-         case 'd':
-             if((x<14)&&maze[y][x+1]!=1){ 
-                x+=1;
-             }
-             break;
+            case 'a':
+                if((aX>1)&&maze[aY][aX-1]!=1){
+                    aX-=1;
+                }
+                break;
+
+            case 'd':
+                if((aX<MAXN-2)&&maze[aY][aX+1]!=1){
+                    aX+=1;
+                }
+                break;
+            case 'q':
+            	puts("Quit~");
+            	exit(0);
         }
     }
-end:
-	printf("\n\nYou Win\nInput any to Quit\n");
-	getch();
+    end:
+    printf("\n\nYou Win!!!\nInput any to Quit\n");
+    getch();
     return 0;
 }
 
