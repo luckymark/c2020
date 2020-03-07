e924630aca7589b63dc1af332a45a1f5b108e050
sorarain <57026826+sorarain@users.noreply.github.com>
2020-03-07 19:40:15 +0800

    Update encrypt_decrypt.cpp

--- level1/p07_encrypt_decrypt/encrypt_decrypt.cpp
+++ level1/p07_encrypt_decrypt/encrypt_decrypt.cpp
@@ -18,40 +18,41 @@ void encrypt(char* s)
 {
 	int len = strlength(s);
 	int i;
-	printf("¼ÓÃÜºóµÄ×Ö·û´®Îª\n");
+	printf("加密后的字符串为\n");
 	for(i = 0;i < len;i++)
-	printf("%c",*(s + i) + 1 + i / 2);
+		printf("%c",*(s + i) + 1 + i / 2);
 	return;
 }
 void decrypt(char* s)
 {
 	int len = strlength(s);
 	int i;
-	printf("Ô­µÄ×Ö·û´®Îª\n");
+	printf("原的字符串为\n");
 	for(i = 0;i < len;i++)
-	printf("%c", *(s + i) - 1 - i / 2);
+		printf("%c", *(s + i) - 1 - i / 2);
 }
 int main(void)
 {
 	while(1)
 	{
 		system("cls");
-		printf("ÇëÑ¡ÔñÒªÊµÏÖµÄ¹¦ÄÜ\n");
-		printf("1¼ÓÃÜ×Ö·û´®\n");
-		printf("2½âÃÜ×Ö·û´®\n");
+		printf("请选择要实现的功能\n");
+		printf("1加密字符串\n");
+		printf("2解密字符串\n");
 		char c;
 		c = getch();
 		if(c != '1' && c!= '2') continue;
 		if(c == '1')
-		printf("ÇëÊäÈëÒª¼ÓÃÜµÄ×Ö·û´®\n");
+			printf("请输入要加密的字符串\n");
 		else if(c == '2')
-		printf("ÇëÊäÈëÒª½âÃÜµÄ×Ö·û´®\n");
+			printf("请输入要解密的字符串\n");
 		char s[200];
 		scanf("%s",s);
 		if(c == '1')
-		encrypt(s);
-		else decrypt(s);
-		printf("\nÇë°´ÈÎÒâ¼ü¼ÌÐø");
+			encrypt(s);
+		else 
+			decrypt(s);
+		printf("\n请按任意键继续");
 		char ch = getch();
 	}
 	return 0;
