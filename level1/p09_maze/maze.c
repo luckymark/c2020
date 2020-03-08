82fe18df61ff78af23a4756d3b43e66780458222
Pluto-wei <2634002591@qq.com>
2020-03-08 22:05:37 +0800

    优化maze.c

--- level1/p09_maze/maze.c
+++ level1/p09_maze/maze.c
@@ -8,14 +8,6 @@
 //辅助宏定义
 #define MAPHIGHT 15
 #define MAPWIDTH 15
-
-char ch;  //按键
-//数据设计
-struct{
-    int x;
-    int y;
-}YourPosition={1,0};
-
 int map[MAPHIGHT][MAPWIDTH]={\
 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,\
 0,0,0,1,1,0,0,0,1,1,1,1,1,1,1,\
@@ -32,18 +24,46 @@ int map[MAPHIGHT][MAPWIDTH]={\
 1,1,1,0,0,1,1,1,0,1,1,1,1,1,1,\
 1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,\
 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,\
-
 };
 
+char ch;  //按键
+struct{
+    int x;
+    int y;
+}YourPosition={1,0};
 
-void gotoxy(int x,int y);
 
-//模块化设计
-//1.画地图 ⊙ ██  可惜命令台输出不了这个符号
-void drawMap(){
+void gotoxy(int x,int y);//光标移动
+void HideCursor(); //隐藏光标
+void drawMap(); //1.画地图
+void keyDown(); //2.按键处理
+int isWin(int x,int y); //3.结束标志
+
+int main(){
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);    
+    SetConsoleTextAttribute(handle,FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE|FOREGROUND_INTENSITY);
+    //system("@color 0f"); //设定的颜色会被最后一个颜色覆盖，只能设定一种颜色
+    drawMap();
+    HideCursor();
+    while(1){
+        if(_kbhit()){  //有按键操作时返回非0
+            keyDown();
+            if(isWin(YourPosition.x,YourPosition.y)){
+                gotoxy(19,0);
+                SetConsoleTextAttribute(handle,FOREGROUND_RED);
+                printf(" Congratulations!!\n\n\n\n\n");
+                break;
+            }
+        }
+    }
+    system("pause");
+    return 0;
+}
+
+void drawMap(){   //⊙ ██ 
     for(int i=0;i<MAPHIGHT;i++){
         for(int j=0;j<MAPWIDTH;j++){
-            if(map[i][j]) printf("#");
+            if(map[i][j]) printf("■");
             else printf(" ");
         }printf("\n");
     }
@@ -53,31 +73,26 @@ void drawMap(){
     printf("You are here '@' , AND '!!' is the exit .");
     gotoxy(13,14);
     printf("!!");
-    
     SetConsoleTextAttribute(handle,FOREGROUND_GREEN|FOREGROUND_INTENSITY);
     gotoxy(1,0);
     printf("@");  //操作者
 }
-//2.按键处理
+
 void keyDown(){
-    int tempx,tempy;
+    int tempx=YourPosition.x,tempy=YourPosition.y;
     ch=getch();
     switch(ch){
         case 'w':
-            tempx=YourPosition.x-1;
-            tempy=YourPosition.y;
+            tempx--;
             break;
         case 's':
-            tempx=YourPosition.x+1;
-            tempy=YourPosition.y;
+            tempx++;
             break;
         case 'a':
-            tempx=YourPosition.x;
-            tempy=YourPosition.y-1;
+            tempy--;
             break;
         case 'd':
-            tempx=YourPosition.x;
-            tempy=YourPosition.y+1;
+            tempy++;
             break;
         default:
             printf("\a");
@@ -97,14 +112,13 @@ void keyDown(){
     YourPosition={tempx,tempy};
     return;
 }
-//3.结束标志
+
 int isWin(int x,int y){
     if(x==13&&y==14){
         return 1;
     }else return 0;
 }
 
-//光标移动
 void gotoxy(int x,int y){
     //调用win32 API去设置控制台的光标位置
     //找到控制台的这个窗口
@@ -124,26 +138,4 @@ void HideCursor() //隐藏光标
 	cursor.dwSize = sizeof(cursor);    
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);    
 	SetConsoleCursorInfo(handle, &cursor);
-}
-
-int main(){
-    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);    
-    SetConsoleTextAttribute(handle,FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE|FOREGROUND_INTENSITY);
-    //system("@color 0f"); //设定的颜色会被最后一个颜色覆盖，只能设定一种颜色
-    drawMap();
-    HideCursor();
-    while(1){
-        if(_kbhit()){  //有按键操作时返回非0
-            keyDown();
-            if(isWin(YourPosition.x,YourPosition.y)){
-                gotoxy(19,0);
-                SetConsoleTextAttribute(handle,FOREGROUND_RED);
-                printf(" Congratulations!!\n\n\n\n\n");
-                break;
-            }
-        }
-    }
-
-    system("pause");
-    return 0;
 }
