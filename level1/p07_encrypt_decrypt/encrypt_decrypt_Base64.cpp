702ebf2f539f7e49e0e18f9345aa6610afbcde03
MinatoXeon <15091591733@126.com>
2020-03-09 17:45:32 +0800

    正在准备与Base64加密与解码的相关知识

---
+++ level1/p07_encrypt_decrypt/encrypt_decrypt_Base64.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define For(x,a,b) for(x=a;x<=b;x++)
+int i,i1,i2;
+int main(void)
+{
+	
+	system("pause");
+	return 0;
+}
