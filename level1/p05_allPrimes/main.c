f28b6e1ab90fed546c96663b12cd76698728a26b
ουονοωο <ouovowo@outlook.com>
2020-03-13 23:34:05 +0800

    Update main.c

--- level1/p05_allPrimes/main.c
+++ level1/p05_allPrimes/main.c
@@ -1,20 +1,33 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
 #define MAXN 1000
+
+void allPrimes(int echo);
 
 int main() {
 	system("title allPrimes");
+	clock_t t0 = clock();
+	allPrimes(1);
+	printf("\nTime cost (with echo on ) = %fs"  , (double)(clock() - t0) / CLOCKS_PER_SEC);
+	t0 = clock();
+	allPrimes(0);
+	printf("\nTime cost (with echo off) = %fs\n", (double)(clock() - t0) / CLOCKS_PER_SEC);
+	system("pause");
+	return 0;
+}
 
+void allPrimes(int echo) {
 	int book[MAXN + 1] = {0};
 	for (int i = 2; i <= MAXN; i++) {
 		if (!book[i]) {
-			printf("%d\n", i);
+			if(echo) {
+				printf("%5d", i);
+			}
 			for (int j = i + i; j <= MAXN; j += i) {
 				book[j] = 1;
 			}
 		}
 	}
-
-	system("pause");
-	return 0;
 }
