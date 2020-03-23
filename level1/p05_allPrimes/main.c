8af5146a1f7d2ac3d86c77a266cb33a86539877b
zhangchangshuo <3312280576@qq.com>
2020-03-24 01:39:19 +0800

    Perfect the all primes

--- level1/p05_allPrimes/main.c
+++ level1/p05_allPrimes/main.c
@@ -17,15 +17,17 @@ void judge(int wolka)
 }
 
 int main() {
-    long start, end;
+    clock_t start, end;
+    double time;
     int i;
     start = clock();
     prime[0] = 2;
-    for (i = 3; i <= 1000; i++) judge(i);
+    for (i = 3; i <= 1000; i=i+2) judge(i);
     for (int j = 0; j <= n; j++)
         printf("%d\n", prime[j]);
     end = clock();
-    printf("Running Time:%ldms\n", end - start);
+    time=(double)(end-start)/CLOCKS_PER_SEC;
+    printf("Running Time:%fms\n",time);
     getchar();
     return 0;
 }
