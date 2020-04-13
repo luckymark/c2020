3a964d56b71c83067771ae919f60abf5d34e8509
abigarrtist <2069460766@qq.com>
2020-04-13 20:45:01 +0800

    TEXt

--- level1/p08_hanoi/hanoi.c
+++ level1/p08_hanoi/hanoi.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int cnt=0;
-void f(int n,char x,char y){
+void f(int n,char x,char y,char z){
     if(n<1) return;
-    f(n-1,'A','B');
-    printf("%c->%c\n",x,y);
+    f(n-1,x,z,y);
+    printf("%c->%c\n",x,z);
     cnt++;
-    f(n-1,'B','C');
+    f(n-1,y,x,z);
 }
 int main() {
     int n;
     scanf("%d",&n);
-    f(n,'A','C');
+    f(n,'A','B','C');
     printf("%d",cnt);
     return 0;
 }
