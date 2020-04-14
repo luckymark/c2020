db21a59df9e3258cdee6780c92319cf2c57ab7c8
SMagic-L <38354551+SMagic-L@users.noreply.github.com>
2020-04-14 12:23:41 +0800

    Complete level1, P09 and P12 remain undebugged

--- level1/p09_maze/maze.c
+++ level1/p09_maze/maze.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "conio.h"
 #include <time.h>
+#include <string.h>
 
 #define MAX_SIZE 99
 
@@ -31,12 +32,23 @@ typedef struct
     Point point, startPoint, endPoint,mapSize,tmpPoint;
     char standOn;
 } GameEngine;
+typedef struct
+{
+    int set[MAX_SIZE * MAX_SIZE];
+} DisjointSet;
 
-//prototype of object method
+//prototype of point and engine method
 GameEngine *generateEngine(Point mapSize);
 Point pointPlus(Point left, Point right);
 GameEngine * NewEngine(Point mapSize);
 GameEngine *disposeEngine(GameEngine *engine);
+
+//prototype of map generator
+int findSet(DisjointSet *pSet, int x);
+void unionSet(DisjointSet *pSet, int x, int y);
+DisjointSet NewDisjointSet(int count);
+void breakWall(GameEngine *engine, int node, int neighbor);
+int getNeighborNode(int node, int m);
 
 //prototype of game logic
 int isPlayerBlocked(GameEngine *engine);
@@ -108,7 +120,8 @@ void playGame(GameEngine *engine)
     }
 
     //show win message
-
+    printf("\n\n\nCongratulation!\n");
+    system("pause");
 }
 
 void walk(GameEngine *engine)
@@ -148,32 +161,22 @@ GameEngine * generateEngine(Point mapSize)
 
     //init
     GameEngine * engine = NewEngine(mapSize);
+    int nodeCount = engine->mapSize.x * engine->mapSize.y;
+    DisjointSet set = NewDisjointSet(nodeCount);
+    int rdNode,rdNeighbor = 0;
 
     //generate
-
-
-
-    return engine;
-}
-
-Point getSucceededMapSizeFromUser()
-{
-    //init
-    Point mapSize;
-    int m,n;
-
     do
     {
-        system("cls");
-        printf("请输入迷宫的长，最大值%d：",MAX_SIZE);
-        scanf("%d",&m);
-        printf("\n请输入迷宫的宽，最大值%d：",MAX_SIZE);
-        scanf("%d",&n);
-        putchar('\n');
-    } while (m > 0 && n > 0 && m < MAX_SIZE && n < MAX_SIZE);
+        rdNode = getRandomInt(nodeCount-1);
+        rdNeighbor = getNeighborNode(rdNode, engine->mapSize.x);
+        if(findSet(&set,rdNeighbor) == findSet(&set, rdNode))
+            continue;
+        breakWall(engine,rdNode,rdNeighbor);
+        unionSet(&set, rdNode, rdNeighbor);
+    }while (findSet(&set,nodeCount-1) != findSet(&set, 0));
 
-    mapSize = (Point){m, n};
-    return mapSize;
+    return engine;
 }
 
 GameEngine * NewEngine(Point mapSize)
@@ -198,11 +201,99 @@ GameEngine * NewEngine(Point mapSize)
 
     //set Start Node And End Node
     engine->startPoint = (Point){1,0};
-    engine->endPoint = (Point){2*m+1,2*n};
+    engine->endPoint = (Point){2 * m,2 * n + 1};
     engine->map[engine->startPoint.x][engine->startPoint.y] = mapBit[startSignal];
     engine->map[engine->startPoint.x][engine->startPoint.y] = mapBit[endSignal];
 
     return engine;
+}
+
+int getNeighborNode(int node, int m)
+{
+    int rd = getRandomInt(100);
+    if(rd< 25)
+    {
+        return node - 1;
+    } else if (rd >= 25 && rd < 50)
+    {
+        return node + 1;
+    } else if (rd >= 50 && rd < 75)
+    {
+        return node + m;
+    } else
+    {
+        return node - m;
+    }
+}
+
+void breakWall(GameEngine * engine, int node, int neighbor)
+{
+    int i = (node / engine->mapSize.x) + 1 + (neighbor / engine->mapSize.x);
+    int j = (node % engine->mapSize.y) + 1 + (neighbor % engine->mapSize.y);
+    engine->map[i][j] = mapBit[space];
+}
+
+DisjointSet NewDisjointSet(int count)
+{
+    DisjointSet result;
+    memset(&result,0, sizeof(DisjointSet));
+    int i;
+    for(i = 0;i<count;i++)
+        result.set[i] = i;
+    return result;
+}
+
+void unionSet(DisjointSet *pSet, int x, int y)
+{
+    if ((x = findSet(pSet,x)) == (y = findSet(pSet,y)))
+        return;
+    if (pSet->set[x] < pSet->set[y])
+    {
+        pSet->set[x] += pSet->set[y];
+        pSet->set[y] = x;
+    }
+    else
+    {
+        pSet->set[y] += pSet->set[x];
+        pSet->set[x] = y;
+    }
+}
+
+int findSet(DisjointSet *pSet, int x)
+{
+    if(x < 0)
+        return -1;
+
+    int root = x, t;
+    while (pSet->set[root] > 0)
+        root = pSet->set[root];
+    while (x != root)
+    {
+        t = pSet->set[x];
+        pSet->set[x] = root;
+        x = t;
+    }
+    return x;
+}
+
+Point getSucceededMapSizeFromUser()
+{
+    //init
+    Point mapSize;
+    int m,n;
+
+    do
+    {
+        system("cls");
+        printf("请输入迷宫的长，最大值%d：",MAX_SIZE);
+        scanf("%d",&m);
+        printf("\n请输入迷宫的宽，最大值%d：",MAX_SIZE);
+        scanf("%d",&n);
+        putchar('\n');
+    } while (m > 0 && n > 0 && m < MAX_SIZE && n < MAX_SIZE);
+
+    mapSize = (Point){m, n};
+    return mapSize;
 }
 
 MenuState showMainMenu()
