cc2daa62799f976fdce17f87348a2880767711e3
lllllllun <2596652987@qq.com>
2020-04-18 19:46:58 +0800

    我太蔡了

---
+++ level1/p10_pushBoxes/pushBoxes/cmake-build-debug/CMakeFiles/pushBoxes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pushBoxes.dir/main.c.o"
+  "libpushBoxes.dll.a"
+  "pushBoxes.exe"
+  "pushBoxes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pushBoxes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
