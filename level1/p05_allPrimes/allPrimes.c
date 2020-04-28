e98451769fdd67d5cd93635a856892dc1a16b49a
lllllllun <2596652987@qq.com>
2020-04-28 22:51:20 +0800

    补加了运行时间

--- level1/p05_allPrimes/allPrimes.c
+++ level1/p05_allPrimes/allPrimes.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
+
 #define maxn 1000
 
-int main(void)
+int main()
 {
-    char i=2;
-    int a[maxn]={0};
-    for(i=2;i<=maxn;i++){
-        if (a[i-1] == 0){
-            int j;
-            for(j=2;i*j<=maxn;j++){
-                a[i*j-1]=1;
-            }
-            printf("%d\n",i);
+    int prime[maxn];
+    int Prime[maxn];
+    memset(prime,0,sizeof(int)*maxn);
+    int tail = -1;
+    clock_t start,finish;
+    start = clock();
+    //欧拉筛
+    for(int i=2;i<=maxn ;i++){
+        if(prime[i]==0){
+            Prime[++tail] = i;}
+        for(int j=0;j<=tail;j++ ){
+                if(i*Prime[j]>maxn) {break;}
+                prime[i*Prime[j]]=1;
+                if((i%Prime[j]) == 0) {break;}
         }
     }
+    //
+    printf("2是素数\n");
+    for(int k=0;k<=tail;k++){
+        printf("%d\n", Prime[k]);
+    }
+    finish = clock();
+    double time = (double)(finish-start)/CLOCKS_PER_SEC;
+    printf("时间是%0.3fs",time);
 }
