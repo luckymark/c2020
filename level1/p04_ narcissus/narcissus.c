9267b95a785c3da3d52dd10340ba0fdef1848d96
lllllllun <2596652987@qq.com>
2020-04-30 20:30:36 +0800

    hahaha

--- level1/p04_ narcissus/narcissus.c
+++ level1/p04_ narcissus/narcissus.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     for(int i=100;i<1000;i++)
     {
-        if(i==(i/100)^3+((i/10)%10)^3+(i%10)^3)
-            printf("%d\n",i);
+        if(i==pow(i/100,3)+pow((i/10)%10,3)+pow(i%10,3))
+            printf("%d\n", i);
+
     }
     return 0;
 }
